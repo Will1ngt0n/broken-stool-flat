9b2b1db0a24340895f208247d70f269ced63ff25
Sithembiso Leonard Luvuno <39264304+CodeName-Spaz@users.noreply.github.com>
2019-11-28 17:09:21 +0200

    Update Page updated

--- src/app/items-list/items-list.page.ts
+++ src/app/items-list/items-list.page.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsListPage implements OnInit {
 
+  promoUdpate: string;
   constructor() { }
 
   ngOnInit() {
   }
 
+  togglePromo(){
+    var promoUpd = document.getElementsByClassName("del-upd-del") as HTMLCollectionOf <HTMLElement>;
+
+    promoUpd[0].style.display = "flex";
+    this.promoUdpate = "Promote item"
+  }
+  toggleUpdate(){
+    var promoUpd = document.getElementsByClassName("del-upd-del") as HTMLCollectionOf <HTMLElement>;
+
+    promoUpd[0].style.display = "flex";
+    this.promoUdpate = "Update item"
+  }
+  dismissPromo(){
+    var promoUpd = document.getElementsByClassName("del-upd-del") as HTMLCollectionOf <HTMLElement>;
+
+    promoUpd[0].style.display = "none"
+  }
 }
