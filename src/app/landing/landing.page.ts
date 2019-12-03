dc91e955574a3babd20c4950556d8be66b548424
Sithembiso Leonard Luvuno <39264304+CodeName-Spaz@users.noreply.github.com>
2019-12-03 11:43:36 +0200

    side menu functions

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -345,4 +345,26 @@ export class LandingPage implements OnInit {
       this.getKwangaItems()
     })
   }
+  
+  showPendingList(){
+    var historyItems = document.getElementsByClassName("pending-items") as HTMLCollectionOf <HTMLElement>;
+    historyItems[0].style.display = "block"
+  }
+  showHistoryList(){
+    var pendingItems = document.getElementsByClassName("history-items") as HTMLCollectionOf <HTMLElement>;
+    pendingItems[0].style.display = "block"
+  }
+  showInventoryList(){
+    var inventoryItems = document.getElementsByClassName("inventory-items") as HTMLCollectionOf <HTMLElement>;
+    inventoryItems[0].style.display = "block"
+  }
+  dismissList(){
+    var historyItems = document.getElementsByClassName("history-items") as HTMLCollectionOf <HTMLElement>;
+    historyItems[0].style.display = "none";
+    var pendingItems = document.getElementsByClassName("pending-items") as HTMLCollectionOf <HTMLElement>;
+    pendingItems[0].style.display = "none";
+    var inventoryItems = document.getElementsByClassName("inventory-items") as HTMLCollectionOf <HTMLElement>;
+    inventoryItems[0].style.display = "none"
+
+  }
 }
--- src/app/winter-gear/winter-gear.page.ts
+++ src/app/winter-gear/winter-gear.page.ts
@@ -12,4 +12,25 @@ export class WinterGearPage implements OnInit {
   ngOnInit() {
   }
 
+  showPendingList(){
+    var historyItems = document.getElementsByClassName("pending-items") as HTMLCollectionOf <HTMLElement>;
+    historyItems[0].style.display = "block"
+  }
+  showHistoryList(){
+    var pendingItems = document.getElementsByClassName("history-items") as HTMLCollectionOf <HTMLElement>;
+    pendingItems[0].style.display = "block"
+  }
+  showInventoryList(){
+    var inventoryItems = document.getElementsByClassName("inventory-items") as HTMLCollectionOf <HTMLElement>;
+    inventoryItems[0].style.display = "block"
+  }
+  dismissList(){
+    var historyItems = document.getElementsByClassName("history-items") as HTMLCollectionOf <HTMLElement>;
+    historyItems[0].style.display = "none";
+    var pendingItems = document.getElementsByClassName("pending-items") as HTMLCollectionOf <HTMLElement>;
+    pendingItems[0].style.display = "none";
+    var inventoryItems = document.getElementsByClassName("inventory-items") as HTMLCollectionOf <HTMLElement>;
+    inventoryItems[0].style.display = "none"
+
+  }
 }
