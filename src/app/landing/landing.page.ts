7a1319243029197e87137d66d19d473baff2cb96
Will1ngt0n <willington.mnisi@gmail.com>
2020-01-10 13:10:09 +0200

    changed refreshOrderHistory function

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -756,10 +756,18 @@ export class LandingPage implements OnInit {
           console.log(data);
           console.log(userID);
           pendingOrder = {refNo : refNo, details : data, noOfItems: data.product.length}
+          for(let key in this.pendingOrders){
+            if(this.pendingOrders[key].refNo === refNo){
+              let index = Number(key)
+              this.pendingOrders.splice(index, 1)
+            }else if(this.pendingOrders[key].refNo !== refNo){
+              //add = true
+            }
+          }
           let index = this.pendingOrders.indexOf(pendingOrder)
           console.log(index);
           
-          this.pendingOrders.splice(index, 1)
+
 
         }
       }
@@ -854,6 +862,7 @@ export class LandingPage implements OnInit {
             let numberOfItems : Number = 0;
             //console.log(this.history);
             if(closedOrder){
+              if(this.history['details'].refNo === refNo){
                 totalPrice = 0
                 numberOfItems = 0
                 for(let i in closedOrder['details'].orders){
@@ -866,12 +875,10 @@ export class LandingPage implements OnInit {
                 closedOrder['details'].totalPrice = totalPrice
                 closedOrder['details'].numberOfItems = numberOfItems
 
-
-
             ////
             this.history.unshift(closedOrder)
             this.orderHistoryLength = this.history.length
-            
+              }          
           }
         }
       }
