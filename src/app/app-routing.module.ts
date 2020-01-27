7f68cfdbadd41adbe0ef2763c30b3dd200ef5a6a
Sithembiso Leonard Luvuno <39264304+CodeName-Spaz@users.noreply.github.com>
2020-01-27 13:53:41 +0200

    Hosted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './services/auth-guard/auth.guard';
+// import { AuthGuard } from './services/auth-guard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'home/:id', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),},
   { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule) },
-  { path: 'landing', loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule),  canActivate: [AuthGuard] },
-  { path: 'sales-specials', loadChildren: () => import('./sales-specials/sales-specials.module').then( m => m.SalesSpecialsPageModule),  canActivate: [AuthGuard]  },
-  { path: 'kwanga-sub-categories', loadChildren: () => import('./kwanga-sub-categories/kwanga-sub-categories.module').then( m => m.KwangaSubCategoriesPageModule),  canActivate: [AuthGuard]  },{ path: 'summer-gear', loadChildren: () => import('./summer-gear/summer-gear.module').then( m => m.SummerGearPageModule) },
-  { path: 'winter-gear', loadChildren: () => import('./winter-gear/winter-gear.module').then( m => m.WinterGearPageModule),  canActivate: [AuthGuard]  },
-  { path: 'items-list/:id', loadChildren: () => import('./items-list/items-list.module').then( m => m.ItemsListPageModule),  canActivate: [AuthGuard]  },
-  { path: 'sign-in', loadChildren: () => import('./sign-in/sign-in.module').then( m => m.SignInPageModule),  canActivate: [AuthGuard]  },
-  { path: 'order-receipt', loadChildren: () => import('./order-receipt/order-receipt.module').then( m => m.OrderReceiptPageModule),  canActivate: [AuthGuard]  },
-  { path: 'pending-order', loadChildren: () => import('./pending-order/pending-order.module').then( m => m.PendingOrderPageModule),  canActivate: [AuthGuard]  },
+  { path: 'landing', loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule) },
+  { path: 'sales-specials', loadChildren: () => import('./sales-specials/sales-specials.module').then( m => m.SalesSpecialsPageModule) },
+  { path: 'kwanga-sub-categories', loadChildren: () => import('./kwanga-sub-categories/kwanga-sub-categories.module').then( m => m.KwangaSubCategoriesPageModule)  },{ path: 'summer-gear', loadChildren: () => import('./summer-gear/summer-gear.module').then( m => m.SummerGearPageModule) },
+  { path: 'winter-gear', loadChildren: () => import('./winter-gear/winter-gear.module').then( m => m.WinterGearPageModule) },
+  { path: 'items-list/:id', loadChildren: () => import('./items-list/items-list.module').then( m => m.ItemsListPageModule) },
+  { path: 'sign-in', loadChildren: () => import('./sign-in/sign-in.module').then( m => m.SignInPageModule)  },
+  { path: 'order-receipt', loadChildren: () => import('./order-receipt/order-receipt.module').then( m => m.OrderReceiptPageModule)  },
+  { path: 'pending-order', loadChildren: () => import('./pending-order/pending-order.module').then( m => m.PendingOrderPageModule)  },
   // { path: '**', redirectTo: 'login', pathMatch: 'full' }
 
 ];
