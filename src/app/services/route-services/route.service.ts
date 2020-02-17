18b4a04f348afb2ec8b71972b7b9a591ea7d20cd
Will1ngt0n <willington.mnisi@gmail.com>
2020-02-17 15:03:19 +0200

    added status to order receipt

--- src/app/services/route-services/route.service.ts
+++ src/app/services/route-services/route.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {LocalStorageService} from 'ngx-webstorage';
+import { LocalStorageService } from 'ngx-webstorage';
 import { Location } from '@angular/common'
 @Injectable({
   providedIn: 'root'
